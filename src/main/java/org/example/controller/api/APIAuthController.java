a5e7deb7962aba0ba9a482e079ca5ef3fefac701
yunhoJ <wjse213@coontec.com>
2022-10-24 21:55:59 +0900

    initial-api

---
+++ src/main/java/org/example/controller/api/APIAuthController.java
@@ -0,0 +1,20 @@
+package org.example.controller.api;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/api")
+@RestController
+public class APIAuthController {
+
+    @GetMapping("/sign-up")
+    public String signUp(){
+        return "done";
+    }
+
+    @GetMapping("/login")
+    public String login(){
+        return "done";
+    }
+}
---
+++ src/main/java/org/example/controller/api/APIPlaceController.java
@@ -0,0 +1,35 @@
+package org.example.controller.api;
+
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RequestMapping("/api")
+@RestController
+public class APIPlaceController {
+    
+    @GetMapping("/places")
+    public List<String> getPlaces(){
+        return List.of("Place1","Place2");
+    }
+    
+    @PostMapping("/places")
+    public Boolean createPlace(){
+        return true;
+    }
+    
+    @GetMapping("places/{placeId}")
+    public String getPlace(@PathVariable Integer placeId){
+        return "Place"+placeId;
+    }
+
+    @PutMapping("places/{placeId}")
+    public Boolean modifyPlace(@PathVariable Integer placeId){
+        return true;
+    }
+    
+    @DeleteMapping("/places/{placeId}")
+    public Boolean removePlace(@PathVariable Integer placeId){
+        return true;
+    }
+}
