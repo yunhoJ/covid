f9663c6334f66788a306f13b3ceed013ff07727a
yunhoJ <wjse213@coontec.com>
2022-10-26 19:12:16 +0900

    mock perform - when & then

---
+++ src/test/java/org/example/controller/BaseControllerTest.java
@@ -0,0 +1,33 @@
+package org.example.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@AutoConfigureMockMvc //mock 설정을 자동으로 해준다
+@SpringBootTest
+class BaseControllerTest {
+    @Autowired//자동으로 주입
+    private MockMvc mvc;
+    @Test
+    void root() throws Exception {
+        ///Given
+
+        //When &  Then
+        mvc.perform(get("/"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
+                .andExpect(content().string(containsString("스프링")))
+                .andExpect(view().name("index"))
+                .andDo(print());
+    }
+}
