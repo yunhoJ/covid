b3e15d6d87115cac558e69d0aed4c46b1d87f343
yunhoJ <wjse213@coontec.com>
2022-10-26 19:33:01 +0900

    field ->method 주입

--- src/test/java/org/example/controller/BaseControllerTest.java
+++ src/test/java/org/example/controller/BaseControllerTest.java
@@ -1,12 +1,12 @@
 package org.example.controller;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import static org.hamcrest.CoreMatchers.containsString;
@@ -17,16 +17,18 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @AutoConfigureMockMvc //mock 설정을 자동으로 해준다
 @SpringBootTest
 class BaseControllerTest {
-    @Autowired//자동으로 주입
-    private MockMvc mvc;
+//    @Autowired//자동으로 주입
+//    private MockMvc mvc;
+
+    @DisplayName("[view][GET] basePage requirement")
     @Test
-    void root() throws Exception {
+    void testRoot(@Autowired  MockMvc mvc) throws Exception {
         ///Given
 
         //When
-        ResultActions result= mvc.perform(get("/"));
+
         //Then
-        result
+        mvc.perform(get("/"))
                 .andExpect(status().isOk())
                 .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
                 .andExpect(content().string(containsString("스프링")))
