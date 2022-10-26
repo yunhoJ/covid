e61ee32a51b2dfd002dbd10c2c52b9c2a272ac8c
yunhoJ <wjse213@coontec.com>
2022-10-26 19:45:24 +0900

    webmvctest

--- src/test/java/org/example/controller/BaseControllerTest.java
+++ src/test/java/org/example/controller/BaseControllerTest.java
@@ -3,8 +3,7 @@ package org.example.controller;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -13,8 +12,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-@AutoConfigureMockMvc //mock 설정을 자동으로 해준다
-@SpringBootTest
+@WebMvcTest(BaseControllerTest.class)//테스트 하고 싶은 대상만
 class BaseControllerTest {
 
     private final MockMvc mvc;
